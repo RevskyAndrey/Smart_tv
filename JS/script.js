1f64538556edaf48a50e80bc9ebb1ffb3896f47f
RevskyAndrey <andrey.g1362@gmail.com>
2019-05-25 22:55:32 +0300

    fix error remove img

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,24 @@
 ﻿'use strict';
 document.addEventListener('DOMContentLoaded', function () {
 
-    // function timePrint() {
-    //     var Data = new Date();
-    //     var Hour = Data.getHours();
-    //     var Minutes = Data.getMinutes();
-    //     var Seconds = Data.getSeconds();
-    //
-    //     Minutes = correctTime(Minutes);
-    //     Seconds = correctTime(Seconds);
-    //     // Вывод
-    //     // document.write(Hour+":"+Minutes+":"+Seconds);
-    //     document.getElementById("clock").innerHTML = Hour + ":" + Minutes + ":" + Seconds;
-    //
-    //     var t = setInterval(timePrint, 1000);
-    // };
+    function timePrint() {
+        let Data = new Date();
+        let Hour = Data.getHours();
+        let Minutes = Data.getMinutes();
+        let Seconds = Data.getSeconds();
+        let Year = Data.getFullYear();
+        let Month = Data.getMonth();
+        let Day = Data.getDate();
+
+        Minutes = correctTime(Minutes);
+        Seconds = correctTime(Seconds);
+        Month = switchMonth(Month);
+
+        document.getElementById("clock").innerHTML = Hour + ":" + Minutes + ":" + Seconds;
+        document.getElementById("date").innerHTML = Day + " " + Month + " " + Year + " г.";
+        let t = setInterval(timePrint, 1000);
+    }
+
 
     function correctTime(i) {
         var i;
@@ -25,59 +29,50 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
 
-//     function dataPrint() {
-//         Data = new Date();
-//         Year = Data.getFullYear();
-//         Month = Data.getMonth();
-//         Day = Data.getDate();
-//
-// // Преобразуем месяца
-//         switch (Month) {
-//             case 0:
-//                 fMonth = "января";
-//                 break;
-//             case 1:
-//                 fMonth = "февраля";
-//                 break;
-//             case 2:
-//                 fMonth = "марта";
-//                 break;
-//             case 3:
-//                 fMonth = "апреля";
-//                 break;
-//             case 4:
-//                 fMonth = "мая";
-//                 break;
-//             case 5:
-//                 fMonth = "июня";
-//                 break;
-//             case 6:
-//                 fMonth = "июля";
-//                 break;
-//             case 7:
-//                 fMonth = "августа";
-//                 break;
-//             case 8:
-//                 fMonth = "сентября";
-//                 break;
-//             case 9:
-//                 fMonth = "октября";
-//                 break;
-//             case 10:
-//                 fMonth = "ноября";
-//                 break;
-//             case 11:
-//                 fMonth = "декабря";
-//                 break;
-//         }
-//
-// // Вывод
-// //document.write(Day+" "+fMonth+" "+Year+" г.");
-//
-//         document.getElementById("date").innerHTML = Day + " " + fMonth + " " + Year + " г.";
-//     };
+    function switchMonth(Month) {
+        switch (Month) {
+            case 0:
+                Month = "января";
+                break;
+            case 1:
+                Month = "февраля";
+                break;
+            case 2:
+                Month = "марта";
+                break;
+            case 3:
+                Month = "апреля";
+                break;
+            case 4:
+                Month = "мая";
+                break;
+            case 5:
+                Month = "июня";
+                break;
+            case 6:
+                Month = "июля";
+                break;
+            case 7:
+                Month = "августа";
+                break;
+            case 8:
+                Month = "сентября";
+                break;
+            case 9:
+                Month = "октября";
+                break;
+            case 10:
+                Month = "ноября";
+                break;
+            case 11:
+                Month = "декабря";
+                break;
+        }
+        return Month;
+    }
 
-
-});
+    timePrint()
+})
+;
 
 
